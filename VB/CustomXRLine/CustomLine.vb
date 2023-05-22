e88ce7186f9a6139ad13e57795748552068a9f61
DevExpressExampleBot <fakeEmail>
2023-05-22 22:31:57 +0400

    Source auto update [skip ci]

--- VB/CustomXRLine/CustomLine.vb
+++ VB/CustomXRLine/CustomLine.vb
@@ -1,84 +1,92 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Text
 Imports DevExpress.XtraReports.UI
 Imports DevExpress.XtraPrinting
 Imports System.Drawing
 
 Namespace CustomXRLine
-	Public Class CustomLine
-		Inherits XRLine
-		Private dash_Renamed As Integer
-		Private space_Renamed As Integer
-		Public Sub New()
-			MyBase.New()
-			Me.dash_Renamed =1
-			Me.space_Renamed = 0
-		End Sub
-		<DisplayName("Dash size"), Browsable(True), DefaultValue(1)> _
-		Public Property Dash() As Integer
-			Get
-				Return Me.dash_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				Me.dash_Renamed = value
-			End Set
-		End Property
-		<DisplayName("Space size"), Browsable(True), DefaultValue(0)> _
-		Public Property Space() As Integer
-			Get
-				Return Me.space_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				Me.space_Renamed = value
-			End Set
-		End Property
-		Protected Overrides Function CreateBrick(ByVal childrenBricks() As VisualBrick) As VisualBrick
-			If Me.LineStyle <> System.Drawing.Drawing2D.DashStyle.Custom Then
-				Return MyBase.CreateBrick(childrenBricks)
-			Else
-				Return New PanelBrick(Me)
-			End If
-		End Function
 
-		Protected Overrides Sub PutStateToBrick(ByVal brick As VisualBrick, ByVal ps As PrintingSystemBase)
-			If Me.LineStyle <> System.Drawing.Drawing2D.DashStyle.Custom Then
-				MyBase.PutStateToBrick(brick, ps)
-			Else
-				Dim panel As PanelBrick = CType(brick, PanelBrick)
-				panel.BackColor = Color.Transparent
-				panel.Sides = BorderSide.None
-				Dim i As Integer
-				If Me.LineDirection = LineDirection.Horizontal Then
-					For i = 0 To Convert.ToInt32(panel.Rect.Width / (Me.Dash + Me.Space)) - 1
-						Dim dashBrick As New LineBrick()
-						dashBrick.Sides = BorderSide.None
-						dashBrick.BackColor = panel.Style.ForeColor
-                        dashBrick.Rect = New RectangleF(i * (Me.Dash + Me.Space), Convert.ToSingle(panel.Rect.Height) / 2 - Convert.ToSingle(Me.LineWidth) / 2, Me.dash_Renamed, Me.LineWidth)
-						panel.Bricks.Add(dashBrick)
-					Next i
-					Dim endBrick As New LineBrick()
-					endBrick.Sides = BorderSide.None
-					endBrick.BackColor = panel.Style.ForeColor
-                    endBrick.Rect = New RectangleF(i * (Me.Dash + Me.Space), Convert.ToSingle(panel.Rect.Height) / 2 - Convert.ToSingle(Me.LineWidth) / 2, panel.Rect.Width - i * (Me.Dash + Me.Space), Me.LineWidth)
-					panel.Bricks.Add(endBrick)
-				ElseIf Me.LineDirection = LineDirection.Vertical Then
-					For i = 0 To Convert.ToInt32(panel.Rect.Height / (Me.Dash + Me.Space)) - 1
-						Dim dashBrick As New LineBrick()
-						dashBrick.Sides = BorderSide.None
-						dashBrick.BackColor = panel.Style.ForeColor
-                        dashBrick.Rect = New RectangleF(Convert.ToSingle(panel.Rect.Width) / 2 - Convert.ToSingle(Me.LineWidth) / 2, i * (Me.Dash + Me.Space), Me.LineWidth, Me.Dash)
-						panel.Bricks.Add(dashBrick)
-					Next i
-					Dim endBrick As New LineBrick()
-					endBrick.Sides = BorderSide.None
-					endBrick.BackColor = panel.Style.ForeColor
-                    endBrick.Rect = New RectangleF(Convert.ToSingle(panel.Rect.Width) / 2 - Convert.ToSingle(Me.LineWidth) / 2, i * (Me.Dash + Me.Space), Me.LineWidth, panel.Rect.Height - i * (Me.Dash + Me.Space))
-					panel.Bricks.Add(endBrick)
-				End If
-			End If
-		End Sub
-	End Class
+    Public Class CustomLine
+        Inherits XRLine
+
+        Private dashField As Integer
+
+        Private spaceField As Integer
+
+        Public Sub New()
+            MyBase.New()
+            dashField = 1
+            spaceField = 0
+        End Sub
+
+        <DisplayName("Dash size"), Browsable(True), DefaultValue(1)>
+        Public Property Dash As Integer
+            Get
+                Return dashField
+            End Get
+
+            Set(ByVal value As Integer)
+                dashField = value
+            End Set
+        End Property
+
+        <DisplayName("Space size"), Browsable(True), DefaultValue(0)>
+        Public Property Space As Integer
+            Get
+                Return spaceField
+            End Get
+
+            Set(ByVal value As Integer)
+                spaceField = value
+            End Set
+        End Property
+
+        Protected Overrides Function CreateBrick(ByVal childrenBricks As VisualBrick()) As VisualBrick
+            If LineStyle <> System.Drawing.Drawing2D.DashStyle.Custom Then
+                Return MyBase.CreateBrick(childrenBricks)
+            Else
+                Return New PanelBrick(Me)
+            End If
+        End Function
+
+        Protected Overrides Sub PutStateToBrick(ByVal brick As VisualBrick, ByVal ps As PrintingSystemBase)
+            If LineStyle <> System.Drawing.Drawing2D.DashStyle.Custom Then
+                MyBase.PutStateToBrick(brick, ps)
+            Else
+                Dim panel As PanelBrick = CType(brick, PanelBrick)
+                panel.BackColor = Color.Transparent
+                panel.Sides = BorderSide.None
+                Dim i As Integer
+                If LineDirection = LineDirection.Horizontal Then
+                    For i = 0 To Convert.ToInt32(panel.Rect.Width / (Dash + Space)) - 1
+                        Dim dashBrick As LineBrick = New LineBrick()
+                        dashBrick.Sides = BorderSide.None
+                        dashBrick.BackColor = panel.Style.ForeColor
+                        dashBrick.Rect = New RectangleF(i * (Dash + Space), panel.Rect.Height / 2 - LineWidth \ 2, dashField, LineWidth)
+                        panel.Bricks.Add(dashBrick)
+                    Next
+
+                    Dim endBrick As LineBrick = New LineBrick()
+                    endBrick.Sides = BorderSide.None
+                    endBrick.BackColor = panel.Style.ForeColor
+                    endBrick.Rect = New RectangleF(i * (Dash + Space), panel.Rect.Height / 2 - LineWidth \ 2, panel.Rect.Width - i * (Dash + Space), LineWidth)
+                    panel.Bricks.Add(endBrick)
+                ElseIf LineDirection = LineDirection.Vertical Then
+                    For i = 0 To Convert.ToInt32(panel.Rect.Height / (Dash + Space)) - 1
+                        Dim dashBrick As LineBrick = New LineBrick()
+                        dashBrick.Sides = BorderSide.None
+                        dashBrick.BackColor = panel.Style.ForeColor
+                        dashBrick.Rect = New RectangleF(panel.Rect.Width / 2 - LineWidth \ 2, i * (Dash + Space), LineWidth, Dash)
+                        panel.Bricks.Add(dashBrick)
+                    Next
+
+                    Dim endBrick As LineBrick = New LineBrick()
+                    endBrick.Sides = BorderSide.None
+                    endBrick.BackColor = panel.Style.ForeColor
+                    endBrick.Rect = New RectangleF(panel.Rect.Width / 2 - LineWidth \ 2, i * (Dash + Space), LineWidth, panel.Rect.Height - i * (Dash + Space))
+                    panel.Bricks.Add(endBrick)
+                End If
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/CustomXRLine/Form1.vb
+++ VB/CustomXRLine/Form1.vb
@@ -1,26 +1,27 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Drawing.Design
 Imports System.Windows.Forms
 Imports DevExpress.XtraReports.UI
+
 ' ...
-
 Namespace CustomXRLine
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Dim report As New XtraReport1()
-			AddHandler report.DesignerLoaded, AddressOf report_DesignerLoaded
-			report.ShowDesigner()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub report_DesignerLoaded(ByVal sender As Object, ByVal e As DevExpress.XtraReports.UserDesigner.DesignerLoadedEventArgs)
-			Dim tb As IToolboxService = TryCast(e.DesignerHost.GetService(GetType(IToolboxService)), IToolboxService)
-			tb.AddToolboxItem(New ToolboxItem(GetType(CustomLine)))
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim report As XtraReport1 = New XtraReport1()
+            AddHandler report.DesignerLoaded, New DevExpress.XtraReports.UserDesigner.DesignerLoadedEventHandler(AddressOf report_DesignerLoaded)
+            report.ShowDesigner()
+        End Sub
+
+        Private Sub report_DesignerLoaded(ByVal sender As Object, ByVal e As DevExpress.XtraReports.UserDesigner.DesignerLoadedEventArgs)
+            Dim tb As IToolboxService = TryCast(e.DesignerHost.GetService(GetType(IToolboxService)), IToolboxService)
+            tb.AddToolboxItem(New ToolboxItem(GetType(CustomLine)))
+        End Sub
+    End Class
 End Namespace
